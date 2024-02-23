26d3ecc1e7871b007d505c75889ae2e5e593a90f
MAHA <maha17p@gmail.com>
2024-02-23 19:48:27 +0530

    created store and person slice

---
+++ redux-toolkit/src/store/features/persons/personSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const initialState = {
+
+}
+
+export const personSlice = createSlice({
+    name:"person",
+    initialState,
+    reducers:{
+
+    },
+});
